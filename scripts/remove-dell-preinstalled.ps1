5776aeb8804a2a50630ae4be18a49a4098358980
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-08 16:58:55 -0500

    Update remove-dell-preinstalled.ps1

--- scripts/remove-dell-preinstalled.ps1
+++ scripts/remove-dell-preinstalled.ps1
@@ -1,32 +1,40 @@
 Start-Transcript -Path $env:TEMP\MSTeamsPersonalRemove.log
 try{
-	If ($null -ne (Get-AppxPackage -Name MicrosoftTeams -AllUsers)) {
-	Write-Output "Removing MicrosoftTeams AppxPackage"
-	Get-AppxPackage -Name MicrosoftTeams -AllUsers | Remove-AppPackage -AllUsers
+	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Core Services')) {
+	Write-Output 'Removing Dell Core Services Package'
+	Uninstall-Package -ProviderName msi -Name 'Dell Core Services'
 }
 	Else {
-	Write-Output "MicrosoftTeams AppxPackage not present"
+	Write-Output 'Dell Core Services Package not present'
 }
-	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'MicrosoftTeams'})) {
-	Write-Output "Removing MicrosoftTeams AppxProvisionedPackage"
-	Remove-AppxProvisionedPackage -online -Packagename MicrosoftTeams -AllUsers
+	#Tested on Dell Optimizer Core 4.1.351.0,4.0.310.0 and Dell Optimizer Service
+	If (Test-Path 'C:\Program Files (x86)\InstallShield Installation Information\{286A9ADE-A581-43E8-AA85-6F5D58C7DC88}\DellOptimizer.exe'){
+	Write-Output 'Removing Dell Optimizer Core/Dell Optimizer Service'
+	&'C:\Program Files (x86)\InstallShield Installation Information\{286A9ADE-A581-43E8-AA85-6F5D58C7DC88}\DellOptimizer.exe' -remove -runfromtemp -silent
 }
 	Else{
-	Write-Output "MicrosoftTeams AppxProvisioned Package not present"
+	Write-Output "Dell Optimizer Core/Service not present"
 }
-	If ($null -ne (Get-AppxPackage -Name MSTeams -AllUsers)) {
+	If (Test-Path 'C:\Program Files\Dell\Dell Display Manager 2.0\uninst.exe') {
 	Write-Output "Removing MSTeams AppxPackage"
-	Get-AppxPackage -Name MSTeams -AllUsers | Remove-AppPackage -AllUsers
+	&'C:\Program Files\Dell\Dell Display Manager 2.0\uninst.exe' /S
 }
 	Else{
-	Write-Output "MSTeams AppxPackage not present"
+	Write-Output 'Dell Display Manager 2.0 not present'
 }
-	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'MSTeams'})) {
-	Write-Output "Removing MSTeams AppxProvisionedPackage"
-	Remove-AppxProvisionedPackage -online -Packagename MSTeams -AllUsers
+	If (Test-Path 'C:\Program Files\Dell\Dell Display Manager 2\uninst.exe') {
+	Write-Output "Removing MSTeams AppxPackage"
+	&'C:\Program Files\Dell\Dell Display Manager 2\uninst.exe' /S
 }
 	Else{
-	Write-Output "MSTeams AppxProvisionedPackage not present"
+	Write-Output 'Dell Display Manager 2 not present'
+}
+	If (Test-Path 'C:\Program Files\Dell\Dell Peripheral Manager\Uninstall.exe') {
+	Write-Output "Removing MSTeams AppxPackage"
+	&'C:\Program Files\Dell\Dell Peripheral Manager\Uninstall.exe' /S
+}
+	Else{
+	Write-Output 'Dell Peripheral Manager not present'
 }
 }
 catch{
