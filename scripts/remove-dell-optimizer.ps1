91a0b75e8d719e814245ecdfca04e6f7b30f8d34
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-08 23:59:54 -0500

    Update remove-dell-optimizer.ps1

--- scripts/remove-dell-optimizer.ps1
+++ scripts/remove-dell-optimizer.ps1
@@ -42,7 +42,7 @@ try{
 	Write-Output 'DellOptimizerUI Package not present'
 }
 	#Tested on Dell Display Manager 2.0.0.137, 2.0.0.139
-	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Display Manager 2.0') {
+	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Display Manager 2.0')) {
 	Write-Output "Removing Dell Display Manager 2.0 Package"
 	&'C:\Program Files\Dell\Dell Display Manager 2.0\uninst.exe' /S | Out-Null
 }
@@ -50,7 +50,7 @@ try{
 	Write-Output 'Dell Display Manager 2.0 not present'
 }
 	#Tested on Dell Display Manager 2.1.1.37
-	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Display Manager 2.1') {
+	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Display Manager 2.1')) {
 	Write-Output "Removing Dell Display Manager 2 Package"
 	&'C:\Program Files\Dell\Dell Display Manager 2\uninst.exe' /S | Out-Null
 }
@@ -58,7 +58,7 @@ try{
 	Write-Output 'Dell Display Manager 2 not present'
 }
 	#Tested on Dell Peripheral Manager 1.6.1, 1.6.5, 1.6.7
-	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Peripheral Manager') {
+	If ($null -ne (Get-Package -ProviderName Programs -Name 'Dell Peripheral Manager')) {
 	Write-Output "Removing Dell Peripheral Manager Package"
 	&'C:\Program Files\Dell\Dell Peripheral Manager\Uninstall.exe' /S | Out-Null
 }
