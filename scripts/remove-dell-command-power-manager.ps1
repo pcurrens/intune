301684e391a15b50cc212747b6a7193560123e1f
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-09 00:23:43 -0500

    Update remove-dell-command-power-manager.ps1

--- scripts/remove-dell-command-power-manager.ps1
+++ scripts/remove-dell-command-power-manager.ps1
@@ -1,51 +1,34 @@
-Start-Transcript -Path $env:TEMP\remove-dell-command-update.log
+Start-Transcript -Path $env:TEMP\remove-dell-command-power-manager.log
 #using Out-Null to force wait for uninstall
 try{
-	#Tested on Dell Command | Update for Windows Universal 5.1.0, 5.0.0
-	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Command | Update for Windows Universal')) {
-	Write-Output 'Removing Dell Command | Update for Windows Universal Package'
-	Uninstall-Package -ProviderName msi -Name 'Dell Command | Update for Windows Universal' | Out-Null
+	#Tested on Dell Command | Power Manager
+	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Command | Power Manager')) {
+	Write-Output 'Removing Dell Command | Power Manager Package'
+	Uninstall-Package -ProviderName msi -Name 'Dell Command | Power Manager' | Out-Null
 }
 	Else {
-	Write-Output 'Dell Command | Update for Windows Universal not present'
+	Write-Output 'Dell Command | Power Manager not present'
 }
-	#Tested on Dell Command | Update for Windows
-	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Command | Update for Windows')) {
-	Write-Output 'Removing Dell Command | Update for Windows Package'
-	Uninstall-Package -ProviderName msi -Name 'Dell Command | Update for Windows' | Out-Null
-}
-	Else {
-	Write-Output 'Dell Command | Update for Windows Package not present'
-}
-	#Tested on Dell Command | Update 4.4.0, 4.6.0, 5.1.0
-	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Command | Update')) {
-	Write-Output 'Removing Dell Command | Update Package'
-	Uninstall-Package -ProviderName msi -Name 'Dell Command | Update' | Out-Null
+	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Power Manager Service')) {
+	Write-Output 'Removing Dell Power Manager Service Package'
+	Uninstall-Package -ProviderName msi -Name 'Dell Power Manager Service' | Out-Null
 }
 	Else {
-	Write-Output 'Dell Command | Update Package not present'
+	Write-Output 'Dell Power Manager Service Package not present'
 }
-	#Tested on Dell Command | Update for Windows 10 4.3.0
-	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell Command | Update for Windows 10')) {
-	Write-Output 'Removing Dell Command | Update for Windows 10 Package'
-	Uninstall-Package -ProviderName msi -Name 'Dell Command | Update for Windows 10' | Out-Null
-}
-	Else {
-	Write-Output 'Dell Command | Update for Windows 10 Package not present'
-}
-	If ($null -ne (Get-AppxPackage -Name 'DellInc.DellCommandUpdate' -AllUsers)) {
-	Write-Output 'Removing DellInc.DellCommandUpdate Appx Package'
-	Get-AppxPackage -Name 'DellInc.DellCommandUpdate' -AllUsers | Remove-AppxPackage -AllUsers | Out-Null
+	If ($null -ne (Get-AppxPackage -Name 'DellInc.DellPowerManager' -AllUsers)) {
+	Write-Output 'Removing DellInc.DellPowerManager Appx Package'
+	Get-AppxPackage -Name 'DellInc.DellPowerManager' -AllUsers | Remove-AppxPackage -AllUsers | Out-Null
 }
 	Else {
-	Write-Output 'DellInc.DellCommandUpdate Appx Package not present'
+	Write-Output 'DellInc.DellPowerManager Appx Package not present'
 }
-	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellCommandUpdate'})) {
-	Write-Output 'Removing DellInc.DellCommandUpdate AppxProvisioned Package'
-	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellCommandUpdate'} | Remove-AppxProvisionedPackage -Online -AllUsers | Out-Null
+	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellPowerManager'})) {
+	Write-Output 'Removing DellInc.DellPowerManager AppxProvisioned Package'
+	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellPowerManager'} | Remove-AppxProvisionedPackage -Online -AllUsers | Out-Null
 }
 	Else{
-	Write-Output 'DellInc.DellCommandUpdate AppxProvisioned Package not present'
+	Write-Output 'DellInc.DellPowerManager AppxProvisioned Package not present'
 }
 }
 catch{
