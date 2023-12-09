909ae35aab4020b4f6b90e87ad6337064f817ce2
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-08 19:41:12 -0500

    Update remove-dell-supportassist.ps1

--- scripts/remove-dell-supportassist.ps1
+++ scripts/remove-dell-supportassist.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path $env:TEMP\remove-dell-supportassist.log
 #using Out-Null to force wait for uninstall
 try{
-	#Tested on Dell SupportAssist 1.0.248.0, 1.3.371.1
+	#Tested on Dell SupportAssist 3.14.2.45116
 	If ($null -ne (Get-Package -ProviderName msi -Name 'Dell SupportAssist')) {
 	Write-Output 'Removing Dell SupportAssist Package'
 	Uninstall-Package -ProviderName msi -Name 'Dell SupportAssist' | Out-Null
