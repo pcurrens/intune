3472b2b20db05bc1ba5d35388439340b24db63ef
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-08 23:44:03 -0500

    Update remove-dell-partnerpromo.ps1

--- scripts/remove-dell-partnerpromo.ps1
+++ scripts/remove-dell-partnerpromo.ps1
@@ -1,19 +1,19 @@
 Start-Transcript -Path $env:TEMP\remove-dell-supportassist.log
 #using Out-Null to force wait for uninstall
 try{
- If ($null -ne (Get-AppxPackage -Name 'DellInc.DellOptimizer' -AllUsers)) {
-	Write-Output 'Removing DellInc.DellOptimizer Appx Package'
-	Get-AppxPackage -Name 'DellInc.DellOptimizer' -AllUsers | Remove-AppxPackage -AllUsers | Out-Null
+	If ($null -ne (Get-AppxPackage -Name 'DellInc.PartnerPromo' -AllUsers)) {
+	Write-Output 'Removing DellInc.PartnerPromo Appx Package'
+	Get-AppxPackage -Name 'DellInc.PartnerPromo' -AllUsers | Remove-AppxPackage -AllUsers | Out-Null
 }
 	Else {
-	Write-Output 'DellInc.DellOptimizer Appx Package not present'
+	Write-Output 'DellInc.PartnerPromo Appx Package not present'
 }
-	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellOptimizer'})) {
-	Write-Output 'Removing DellInc.DellOptimizer AppxProvisioned Package'
-	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellOptimizer'} | Remove-AppxProvisionedPackage -Online -AllUsers | Out-Null
+	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.PartnerPromo'})) {
+	Write-Output 'Removing DellInc.PartnerPromo AppxProvisioned Package'
+	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.PartnerPromo'} | Remove-AppxProvisionedPackage -Online -AllUsers | Out-Null
 }
 	Else{
-	Write-Output 'DellInc.DellOptimizer AppxProvisioned Package not present'
+	Write-Output 'DellInc.PartnerPromo AppxProvisioned Package not present'
 }
 }
 catch{
