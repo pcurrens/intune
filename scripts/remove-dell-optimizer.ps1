ea18912e16b6cf12e7a5e244a6e11654dfa0d16d
pcurrens <110482302+pcurrens@users.noreply.github.com>
2023-12-08 23:39:27 -0500

    Update remove-dell-optimizer.ps1

--- scripts/remove-dell-optimizer.ps1
+++ scripts/remove-dell-optimizer.ps1
@@ -73,6 +73,20 @@ try{
 	Else{
 	Write-Output 'ExpressConnect Drivers & Services not present'
 }
+	If ($null -ne (Get-AppxPackage -Name 'DellInc.DellOptimizer' -AllUsers)) {
+	Write-Output 'Removing DellInc.DellOptimizer Appx Package'
+	Get-AppxPackage -Name 'DellInc.DellOptimizer' -AllUsers | Remove-AppxPackage -AllUsers | Out-Null
+}
+	Else {
+	Write-Output 'DellInc.DellOptimizer Appx Package not present'
+}
+	If ($null -ne (Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellOptimizer'})) {
+	Write-Output 'Removing DellInc.DellOptimizer AppxProvisioned Package'
+	Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -eq 'DellInc.DellOptimizer'} | Remove-AppxProvisionedPackage -Online -AllUsers | Out-Null
+}
+	Else{
+	Write-Output 'DellInc.DellOptimizer AppxProvisioned Package not present'
+}
 }
 catch{
 	$errMsg = $_.Exception.Message
